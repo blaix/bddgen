e549e3d350efd541903e74b665663adda2e1d22b
Justin Blake <justin@hentzia.com>
2010-11-22 14:02:44 -0500

    Refactor BDDGen::Tasks

--- lib/bddgen/tasks.rb
+++ lib/bddgen/tasks.rb
@@ -1,15 +1,16 @@
 module BDDGen
   class Tasks
     def self.cucumber
-      @cucumber_tasks ||= File.read(
-        File.join(BDDGen::App.source_root, 'rake_tasks', 'cucumber.rb')
-      )
+      @cucumber_tasks ||= read_task_file('cucumber.rb')
     end
 
     def self.rspec
-      @rspec_tasks ||= File.read(
-        File.join(BDDGen::App.source_root, 'rake_tasks', 'rspec.rb')
-      )
+      @rspec_tasks ||= read_task_file('rspec.rb')
+    end
+    
+    private
+    def self.read_task_file(filename)
+      File.read(File.join(BDDGen::App.source_root, 'rake_tasks', filename))
     end
   end
 end
